2ff93915482667b2b9a1d64d9b289a536656a083
AudryLopez <alopez@intellisys.com.do>
2021-12-16 22:47:29 -0400

    card rules

--- src/components/rules/rules.tsx
+++ src/components/rules/rules.tsx
@@ -1,11 +1,16 @@
 import "./rules.css"
+import paper from '../../images/icon-paper.svg'
+import rock from '../../images/icon-rock.svg'
+import scissors from '../../images/icon-scissors.svg'
+import rules from '../../images/image-rules.svg'
+
 export default function Rules(){
     return(
-        <section className="card">
-            <header className="title">RULES</header>
-            <body>
-                
-            </body>
-        </section>
+      <section className="card">
+        <header className="title">RULES</header>
+        <div className="rules">
+          <img className="rules" src={rules}></img>
+        </div>
+      </section>
     )
 }
